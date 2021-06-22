1e0a8ef37707bfd21e1b8c59aa9b847f53c723e1
izidormaklary <maklaryizidor@gmail.com>
2021-06-22 09:08:06 +0200

    first loader on the way

--- Model/StudentLoader.php
+++ Model/StudentLoader.php
@@ -1,10 +1,12 @@
 <?php
-
+require 'connection.php';
+require 'Student.php';
 
 class StudentLoader
 {
     private array $students =[];
-    public function __construct()
+
+    public function loadStudents()
     {
         $pdo= Connection::openConnection();
         $handle = $pdo->prepare('SELECT s.id as id, s.Name as name, c.id as classId, t.id as teacherId, s.email as email FROM student s LEFT JOIN teacher t on s.teacherId=t.id LEFT JOIN class c on s.teacherId=c.teacherId');
@@ -13,6 +15,18 @@ class StudentLoader
         foreach ($students as $s){
             $this->students[]= new Student($s['name'], $s['id'], $s['email'], $s['classId'], $s['teacherId']);
         }
+        var_dump($this->students);
     }
-    public function insertStudent
+    public static function insertStudent($name, $email){
+        $pdo= Connection::openConnection();
+        $handle = $pdo->prepare('INSERT INTO student ( Name, email) VALUES ( :name,  :email)');
+        $handle->bindValue(':name', $name);
+        //$handle->bindValue(':class', $class);
+        //$handle->bindValue(':teacherId', $teacherId);
+        $handle->bindValue(':email',$email);
+        $handle->execute();
+    }
 }
+//StudentLoader::insertStudent("becody", "becode@becode.werp" );
+$student = new StudentLoader();
+$student->loadStudents();
--- index.php
+++ index.php
@@ -8,7 +8,7 @@ require 'Controller/HomepageController.php';
 require 'Controller/InfoController.php';
 
 //you could write a simple IF here based on some $_GET or $_POST vars, to choose your controller
-//this file should never be more than 20 lines of code!
+// this file should never be more than 20 lines of code!
 
 $controller = new HomepageController();
 if(isset($_GET['page']) && $_GET['page'] === 'info') {
